10cfc487276569bf00bde9f09b1c02640f36c1b7
Ankit <ankit11sinha1997@gmail.com>
2026-02-19 22:26:26 +0530

    Fixed connection error in sql query

--- db/init_db.py
+++ db/init_db.py
@@ -5,7 +5,7 @@ def init_db():
     cursor = db.cursor()
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS users (
-            id INT SERIAL PRIMARY KEY,
+            id SERIAL PRIMARY KEY,
             name VARCHAR(100) NOT NULL,
             email VARCHAR(100) NOT NULL UNIQUE
         )
